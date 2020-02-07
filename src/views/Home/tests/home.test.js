d71d3c28acff53d2590a68ebda10f79f660fe946
andy <andrewo@shleeq.com>
2020-02-07 21:58:06 +0100

    tested for empty input after being touched

--- src/views/Home/tests/home.test.js
+++ src/views/Home/tests/home.test.js
@@ -1,6 +1,11 @@
-import {wait, waitForElement, fireEvent, render} from '@testing-library/react';
-import React from 'react';
-import Home from '../HomeContainer';
+import {
+  wait,
+  waitForElement,
+  fireEvent,
+  render
+} from "@testing-library/react";
+import React from "react";
+import Home from "../HomeContainer";
 
 // Test cases
 /**
@@ -10,20 +15,45 @@ import Home from '../HomeContainer';
 
 describe("Home Tests", () => {
   it("should load the document to the DOM with a disabled button", () => {
-    const {queryByText} = render(<Home />);
+    const { queryByText } = render(<Home />);
     const submitButton = queryByText("Complete the form to Submit");
     expect(submitButton).not.toBeNull();
   });
 
   it("should not show errors when the pages load", () => {
-    const {queryByText} = render(<Home />);
+    const { queryByText } = render(<Home />);
     const emptyError = queryByText("Required!");
     expect(emptyError).toBeNull();
   });
 
-  it("should not show errors when the pages load", () => {
-    const {queryByText, queryAllByText} = render(<Home />);
-    // const emptyError = queryByText("Required!");
-    // expect(emptyError).toBeNull();
+  it("should show errors when the fields are touched but empty", async () => {
+    const { queryByLabelText, queryAllByText, debug } = render(<Home />);
+    const fullNameInput = queryByLabelText("Full name");
+    const emailInput = queryByLabelText("Email");
+    const phoneInput = queryByLabelText("Phone number");
+    const passwordInput = queryByLabelText("Password");
+    const confirmPasswordInput = queryByLabelText("Confirm Password");
+    const cardnumberInput = queryByLabelText("Card Number");
+    const expirationDateInput = queryByLabelText("Expiration Date");
+    const pinInput = queryByLabelText("PIN");
+
+    fireEvent.blur(fullNameInput);
+    fireEvent.blur(emailInput);
+    fireEvent.blur(phoneInput);
+    fireEvent.blur(passwordInput);
+    fireEvent.blur(confirmPasswordInput);
+    fireEvent.blur(cardnumberInput);
+    fireEvent.blur(expirationDateInput);
+    fireEvent.blur(pinInput);
+
+    await wait(() => {
+      const emptyErrors = queryAllByText("Required!");
+      expect(emptyErrors.length).toBe(8)
+    });
   });
-})
+
+  it("should confirm that the first name and last name was entered", () => {
+    const { queryByLabelText, queryAllByText, debug } = render(<Home />);
+   
+  })
+});
