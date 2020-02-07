c276c64d9dd202a923d8d48adf3198bd0afa788a
andy <andrewo@shleeq.com>
2020-02-07 22:58:35 +0100

    added tests for wrong and correct emails

--- src/views/Home/tests/home.test.js
+++ src/views/Home/tests/home.test.js
@@ -2,12 +2,14 @@ import {
   wait,
   waitForElement,
   fireEvent,
-  render
+  render,
+  cleanup
 } from "@testing-library/react";
 import React from "react";
 import Home from "../HomeContainer";
 
 describe("Home Tests", () => {
+  afterEach(cleanup);
   it("should load the document to the DOM with a disabled button", () => {
     const { queryByText } = render(<Home />);
     const submitButton = queryByText("Complete the form to Submit");
@@ -55,20 +57,67 @@ describe("Home Tests", () => {
     fireEvent.blur(fullNameInput);
     const nameError = await waitForElement(() => queryByTestId("name-error"));
     await wait(() => {
-      expect(nameError.textContent).toBe("Please enter your full name")
+      expect(nameError.textContent).toBe("Please enter your full name");
     });
   });
 
   it("should confirm that the first name and last name was entered correctly", async () => {
-    const { queryByLabelText, queryByTestId } = render(<Home />);
+    const { queryByLabelText, queryByTestId, queryByText } = render(<Home />);
     const fullNameInput = queryByLabelText("Full name");
     fireEvent.change(fullNameInput, {
       target: { value: "Andy Ogaga" }
     });
     fireEvent.blur(fullNameInput);
     await wait(() => {
-      const nameError = queryByTestId("name-error")
-      expect(nameError).toBeNull()
+      const nameError = queryByTestId("name-error");
+      expect(nameError).toBeNull();
+      const submitButton = queryByText("Complete the form to Submit");
+      expect(submitButton).not.toBeNull();
+    });
+  });
+
+  it("should not accept incorrect email", async () => {
+    const { queryByLabelText, queryByTestId, queryByText } = render(<Home />);
+    const emailInput = queryByLabelText("Email");
+    fireEvent.change(emailInput, {
+      target: { value: "test@email" }
+    });
+    fireEvent.blur(emailInput);
+    const emailError = await waitForElement(() => queryByTestId("email-error"));
+    await wait(() => {
+      expect(emailError.textContent).toBe("Please enter valid email");
+      const submitButton = queryByText("Complete the form to Submit");
+      expect(submitButton).not.toBeNull();
+    });
+  });
+
+  it("should confirm that the email was entered correctly", async () => {
+    const { queryByLabelText, queryByTestId, queryByText } = render(<Home />);
+    const emailInput = queryByLabelText("Email");
+    fireEvent.change(emailInput, {
+      target: { value: "test@email.com" }
+    });
+    fireEvent.blur(emailInput);
+    await wait(() => {
+      const emailError = queryByTestId("email-error");
+      expect(emailError).toBeNull();
+      const submitButton = queryByText("Complete the form to Submit");
+      expect(submitButton).not.toBeNull();
+    });
+  });
+
+  it("should confirm that the email was entered correctly", async () => {
+    const { queryByLabelText, queryByTestId, queryByText } = render(<Home />);
+    const emailInput = queryByLabelText("Email");
+    fireEvent.change(emailInput, {
+      target: { value: "test@email.com" }
+    });
+    fireEvent.blur(emailInput);
+    await wait(() => {
+      const emailError = queryByTestId("email-error");
+      expect(emailError).toBeNull();
+      const submitButton = queryByText("Complete the form to Submit");
+      expect(submitButton).not.toBeNull();
     });
   });
 });
