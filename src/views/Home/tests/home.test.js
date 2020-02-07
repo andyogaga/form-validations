b0b8b97c71d4b09c2d5f4b3a198cd99e5f1b2a94
andy <andrewo@shleeq.com>
2020-02-07 09:28:15 +0100

    started the tests for homepage

---
+++ src/views/Home/tests/home.test.js
@@ -0,0 +1,11 @@
+import {wait, waitForElement, fireEvent, render} from '@testing-library/react';
+import React from 'react';
+import Home from '../HomeContainer';
+
+
+// Integration Tests using Kent Dodds React Testing Library
+describe("Home Tests", () => {
+  test("should load the document to the DOM", () => {
+    // write tests here
+  })
+})
